7ff135486b98c57c524459aabd1dfcfe4a2a8f40
prasanthpalanisamy9500-eng <prasanthpalanisamy9500@gmail.com>
2026-09-24 09:56:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,235 @@
+// 1
+var name = "Prasanth";
+console.log(name);
+
+// 2
+var age = 22;
+console.log(age);
+
+// 3
+var city;
+city = "Salem";
+console.log(city);
+
+// 4
+var number = 20;
+number = 40;
+console.log(number);
+
+// 5
+var course = "Java";
+course = "JavaScript";
+console.log(course);
+
+// 6
+var myName = "Prasanth";
+var myAge = 22;
+var myCity = "Salem";
+console.log(myName);
+console.log(myAge);
+console.log(myCity);
+
+// 7
+var collegeName = "AVS College of Arts and Science";
+console.log(collegeName);
+
+// 8
+var subject = "JavaScript";
+console.log(subject);
+
+// 9
+var num = 10;
+num = 20;
+num = 30;
+num = 40;
+console.log(num);
+
+// 10
+var salary = 20000;
+salary = 25000;
+console.log(salary);
+
+
+// VARIABLES - LET
+
+// 11
+let userName = "Prasanth";
+console.log(userName);
+
+// 12
+let userAge = 22;
+console.log(userAge);
+
+// 13
+let qualification;
+qualification = "B.Com";
+console.log(qualification);
+
+// 14
+let value = 100;
+value = 200;
+console.log(value);
+
+// 15
+let name1 = "Prasanth";
+let qualification1 = "B.Com";
+let  Mylocation = "Salem";
+console.log(name1);
+console.log(qualification1);
+console.log(Mylocation);
+
+// 16
+let courseName = "JavaScript";
+console.log(courseName);
+
+// 17
+let marks = 50;
+marks = 80;
+console.log(marks);
+
+// 18
+let companyName = "Stackly";
+console.log(companyName);
+
+// 19
+let experience = 0;
+console.log(experience);
+
+// 20
+let mobileModel = "Nothing Phone";
+console.log(mobileModel);
+
+
+// VARIABLES - CONST
+
+// 21
+const name2 = "Prasanth";
+console.log(name2);
+
+// 22
+const dateOfBirth = "15-11-2003";
+console.log(dateOfBirth);
+
+// 23
+const company = "Stackly";
+console.log(company);
+
+// 24
+const country = "India";
+console.log(country);
+
+// 25
+const name3 = "Prasanth";
+const age3 = 22;
+const city3 = "Salem";
+console.log(name3);
+console.log(age3);
+console.log(city3);
+
+// 26
+const course2 = "JavaScript";
+console.log(course2);
+
+// 27
+const college = "AVS College";
+console.log(college);
+
+// 28
+const favoriteColor = "Blue";
+console.log(favoriteColor);
+
+// 29
+const employeeId = 101;
+console.log(employeeId);
+
+// 30
+const officeLocation = "Chennai";
+console.log(officeLocation);
+
+
+// PRINTING STATEMENTS
+
+// 31
+console.log(100);
+
+// 32
+console.log("Prasanth");
+
+// 33
+console.log(22);
+console.log("B.Com");
+
+// 34
+console.log("Welcome to JavaScript");
+
+// 35
+console.log("Prasanth");
+console.log(22);
+console.log("Salem");
+console.log("B.Com");
+console.log("JavaScript");
+
+// 36
+let userName2 = "Prasanth";
+console.log(userName2);
+
+// 37
+let userAge2 = 22;
+console.log(userAge2);
+
+// 38
+console.log("Prasanth");
+console.log(22);
+console.log("Salem");
+console.log("B.Com");
+
+
+// POPUP METHODS
+
+// 39
+alert("Welcome to JavaScript");
+
+// 40
+let myName2 = "Prasanth";
+alert(myName2);
+
+// 41
+let enteredName = prompt("What is your name?");
+alert(enteredName);
+
+// 42
+let enteredAge = prompt("What is your age?");
+console.log(enteredAge);
+
+// 43
+let enteredQualification = prompt("What is your qualification?");
+alert(enteredQualification);
+
+// 44
+let enteredCity = prompt("What is your city?");
+console.log(enteredCity);
+
+// 45
+confirm("Do you know JavaScript?");
+
+// 46
+confirm("Do you want to continue?");
+
+// 47
+let personName = prompt("What is your name?");
+let personQualification = prompt("What is your qualification?");
+
+document.writeln("Name: " + personName);
+document.writeln("Qualification: " + personQualification);
+
+
+// document.writeln() + Console Methods
+
+// 48
+let message = "Hello Everyone";
+document.writeln(message);
+
+// 49
+console.log("Prasanth");
+console.warn("B.Com");
+console.error("JavaScript");
